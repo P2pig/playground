80a383298ccc92265c05bfdd983fb0b50d8f90f4
Sean Chang <Sean791022@gmail.com>
2021-07-04 22:15:23 +0800

    Add function add();

--- C/elementary_arithmetic_bit_op.c
+++ C/elementary_arithmetic_bit_op.c
@@ -5,10 +5,24 @@ int minus(int a, int b);
 int multiply(int a, int b);
 int divide(int a, int b);
 
+int add(int a, int b) {
+    /* Intuitive */
+/*
+    while(b != 0){
+        int tmp = a^b;
+        b = (a&b) << 1;
+        a = tmp;
+    }
+*/
+    /* recursive */
+    if(b==0)
+        return a;
+    add(a^b, (a&b)<<1);
+}
 
 int main() {
 
-    printf("hello linux\n");
+    printf("100 + 100 = %d\n", add(100,100));
 
     return 0;
 }
