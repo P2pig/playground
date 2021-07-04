68c72c29379d7d76221b8a0d6f6f32276d9c8ead
Sean Chang <Sean791022@gmail.com>
2021-07-04 21:57:27 +0800

    Add ementary_arithmetic_bit_op.c
    Add prototype functions: add, minus, multiply, divide

---
+++ C/elementary_arithmetic_bit_op.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int add(int a,int b);
+int minus(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
+
+
+int main() {
+
+    printf("hello linux\n");
+
+    return 0;
+}
