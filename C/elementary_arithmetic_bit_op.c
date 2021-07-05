2e1b4d83f83526c8b702828752223f16c1ba6d8b
Sean Chang <Sean791022@gmail.com>
2021-07-05 08:35:31 +0800

    Add function multiply();

--- C/elementary_arithmetic_bit_op.c
+++ C/elementary_arithmetic_bit_op.c
@@ -2,7 +2,7 @@
 
 int add(int a,int b);
 int minus(int a, int b);
-int multiply(int a, int b);
+unsigned int multiply(unsigned int a, unsigned int b);
 int divide(int a, int b);
 
 int add(int a, int b) {
@@ -24,10 +24,25 @@ int minus(int a, int b) {
     add(a,-b);
 }
 
+unsigned int multiply(unsigned int a, unsigned int b) {
+    unsigned int t = 0;
+
+    while (b != 0) {
+        if (b & 1)
+            t = add(a,t);
+
+        a <<= 1;
+        b >>= 1;
+    }
+    return t;
+}
+
+
 int main() {
 
     printf("100 + 100 = %d\n", add(100,100));
     printf("5 - 3 = %d\n", minus(5,3));
+    printf("5 * -5 = %d\n", multiply(5,-5)); 
 
     return 0;
 }
